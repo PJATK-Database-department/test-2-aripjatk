29b985777fb3bd09070d746de133cefff0ed8f90
s22796 <s22796@pjwstk.edu.pl>
2022-06-09 10:36:50 +0200

    Created project, models

---
+++ Test2/Test2/Controllers/OrdersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test2.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class OrdersController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
---
+++ Test2/Test2/Models/Confectionery_ClientOrder.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test2.Models {
+    public class Confectionery_ClientOrder {
+        [Key]
+        public int IdClientOrder { get; set; }
+        public int IdConfectionery { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        [MaxLength(300)]
+        public string Comments { get; set; }
+        [ForeignKey("IdClientOrder")]
+        public virtual ClientOrder ClientOrder { get; set; }
+        [ForeignKey("IdConfectionery")]
+        public virtual Confectionery Confectionery { get; set; }
+    }
+}
