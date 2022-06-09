2012bdf4b2c351bc497d36aa106bfae32610366e
s22796 <s22796@pjwstk.edu.pl>
2022-06-09 10:46:28 +0200

    Add DbContext and migration

---
+++ Test2/Test2/OrdersDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test2.Models;
+
+namespace Test2 {
+    public class OrdersDbContext : DbContext {
+        public OrdersDbContext(DbContextOptions opts) : base(opts) { }
+        public DbSet<Confectionery> Confectionery { get; set; }
+        public DbSet<Client> Client { get; set; }
+        public DbSet<ClientOrder> ClientOrder { get; set; }
+        public DbSet<Confectionery_ClientOrder> Confectionery_ClientOrder { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
--- Test2/Test2/Startup.cs
+++ Test2/Test2/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,6 +22,7 @@ namespace Test2 {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
+            services.AddDbContext<OrdersDbContext>(builder => builder.UseSqlServer("Data Source=db-mssql;Initial Catalog=s22796;Integrated Security=True"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
